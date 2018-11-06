f561dddcbbaa14b24ad20ec58828eeb4a1cf4319
Aluno <Aluno@Digital.net>
2018-11-06 18:48:38 -0200

    path

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser')
 var passport = require('passport')
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
+var path = require('path');
 
 require('./controllers/authController')(passport);
 
@@ -22,15 +23,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // VIEW ENGINE SETUP
-app.set('views','./views');
-app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // EXPRESS SETUP
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 // app.use('/users',usersRouter);
 app.use('/incubadoras',incubadorasRouter);
